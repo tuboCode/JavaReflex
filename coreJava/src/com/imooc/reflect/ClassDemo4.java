0faa49823a2465e0c7a82e1278574a52f795f00d
tuboCode <Administrator@PC-20160918PRFU>
2017-06-02 22:23:53 +0800

    java反射

---
+++ coreJava/src/com/imooc/reflect/ClassDemo4.java
@@ -0,0 +1,14 @@
+package com.imooc.reflect;
+
+public class ClassDemo4 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		ClassUtil.printFieldMessage("hello");
+		System.out.println("=============");
+		ClassUtil.printFieldMessage(new Integer(1));
+	}
+
+}
---
+++ coreJava/src/com/imooc/reflect/ClassDemo5.java
@@ -0,0 +1,14 @@
+package com.imooc.reflect;
+
+public class ClassDemo5 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		ClassUtil.printConMessage("hello");
+		ClassUtil.printConMessage(new Integer(1));
+		
+	}
+
+}
